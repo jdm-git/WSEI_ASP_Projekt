e7ebe32b9f96affb2e01a5dc736cd1fc7411fad5
Kuba <58112825+jdm-git@users.noreply.github.com>
2022-03-04 18:43:56 +0100

    Zad. 1

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -48,6 +48,71 @@ namespace LibApp.Models
                         DurationInMonths = 12,
                         DiscountRate = 20
                     });
+                context.Rentals.AddRange(
+                new Rental
+                {
+                    Customer = new Customer
+                    {
+                        Birthdate = DateTime.Now.AddYears(-10),
+                        HasNewsletterSubscribed = true,
+                        MembershipTypeId = 1,
+                        Name = "customer1"
+                    },
+                    Book = new Book
+                    {
+                        AuthorName = "author1",
+                        DateAdded = DateTime.Now.AddDays(-1),
+                        GenreId = 1,
+                        Name = "name1",
+                        NumberAvailable = 10,
+                        NumberInStock = 10,
+                        ReleaseDate = DateTime.Now.AddDays(-2)
+                    },
+                    DateRented = DateTime.Now.AddDays(-1)
+                },
+                new Rental
+                {
+                    Customer = new Customer
+                    {
+                        Birthdate = DateTime.Now.AddYears(-10),
+                        HasNewsletterSubscribed = true,
+                        MembershipTypeId = 1,
+                        Name = "customer2"
+                    },
+                    Book = new Book
+                    {
+                        AuthorName = "author2",
+                        DateAdded = DateTime.Now.AddDays(-1),
+                        GenreId = 2,
+                        Name = "name2",
+                        NumberAvailable = 10,
+                        NumberInStock = 10,
+                        ReleaseDate = DateTime.Now.AddDays(-2)
+                    },
+                    DateRented = DateTime.Now.AddDays(-1)
+                },
+                new Rental
+                {
+                    Customer = new Customer
+                    {
+                        Birthdate = DateTime.Now.AddYears(-10),
+                        HasNewsletterSubscribed = true,
+                        MembershipTypeId = 3,
+                        Name = "customer3"
+                    },
+                    Book = new Book
+                    {
+                        AuthorName = "author3",
+                        DateAdded = DateTime.Now.AddDays(-1),
+                        GenreId = 3,
+                        Name = "name3",
+                        NumberAvailable = 10,
+                        NumberInStock = 10,
+                        ReleaseDate = DateTime.Now.AddDays(-2)
+                    },
+                    DateRented = DateTime.Now.AddDays(-1)
+                }
+                );
                 context.SaveChanges();
             }
         }
